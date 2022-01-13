127209f4174eafbff8076bc2c64f3bcf30bd3c8d
Jack-Desktop <jackpinkerton@live.com.au>
2022-01-13 13:51:03 +1100

    latest version for album get requests

--- server.js
+++ server.js
@@ -1,48 +1,81 @@
 var express = require('express');
 var fs = require('fs');
+var bodyParser = require('body-parser')
+
 var app = express();
+// create application/json parser
+var jsonParser = bodyParser.json()
+
+app.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
+ });
 
 
-const getAlbums = async () => {
-    const albums = fs.readFile('albums.txt', 'utf8', function (err,data) {
-        if (err) {
-          return console.log(err);
-        }
-        console.log(data);
-        data = data.replace(/ /g, '');
-        data = data.replace('[', '');
-        data = data.replace(']', '');
-        data = data.replace(/"/g, '');
-        data = data.replace(/'/g, '');
-        const albums = data.split(',');
-        console.log(albums);
-        return albums
+const getAlbums = () => {
+   try {
+      let data = fs.readFileSync('albums.txt', 'utf8')
+      console.log(data);
+      data = data.replace(/ /g, '');
+      data = data.replace('[', '');
+      data = data.replace(']', ''); // Need to learn regex to improve this
+      data = data.replace(/"/g, '');
+      data = data.replace(/'/g, '');
+      const albums = data.split(',');
+      console.log('albums to return', albums);
+      return albums;
     
-    });
-    console.log('albums to return', albums);
-    return albums;
+    } catch(err) {
+      console.log(err);
+      return [];
+    }
+    
 }
 
-const getResponse = async () => {
-    const albums = await getAlbums();
+const getResponse = () => {
+    const albums = getAlbums();
     const response = {
         albums: albums
     };
     return response;
 }
 
+const writeToFile = (albums) => {
+   try {
+      fs.writeFileSync('albums.txt', albums);
+      return true;
+   } catch (err) {
+      console.log(err)
+      return false;
+   }
+}
+
 // This responds with "Hello World" on the homepage
 app.get('/albums', async function (req, res) {
    console.log("Got a GET request for local albums");
-   const response = await getResponse();
+   const response = getResponse();
    console.log('albums', response);
    res.send(response);
 })
 
 // This responds a POST request for the homepage
-app.post('/albums', function (req, res) {
-   console.log("Got a POST request for local albums");
-   res.send('Will Create album in future');
+app.post('/albums', jsonParser, function (req, res) {
+   console.log("Got a POST request for local albums", req.body);
+   // get albums
+   let albums = getAlbums()
+   console.log("albums from data", albums[0]);
+   // check if album exists
+   const newAlbum = req.body.name
+   console.log("new album name", newAlbum);
+   // add to albums
+   albums.push(newAlbum);
+   console.log("updated list of albums", albums);
+   // write to file
+   const writeSuccess = writeToFile(albums.toString());
+   console.log(writeSuccess);
+   // return
+   res.send(albums);
 })
 
 // This responds a DELETE request for the /del_user page.
